d8ab03d7b7e1768a033a9d9c2556efbd20a48139
mign0n <onsapozhnikov@gmail.com>
2021-07-08 04:29:47 +0500

    Fixed security

--- inception/inception/settings.py
+++ inception/inception/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--n=&yr9kz=hzfj2s#s(0^6+p2swe!_d4ulcn-t#cqsy8en!9yo'
+SECRET_KEY = environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
