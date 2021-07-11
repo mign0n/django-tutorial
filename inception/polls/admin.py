8cfc54b50e144a165f0058bc5d57244da1d43c2a
mign0n <onsapozhnikov@gmail.com>
2021-07-11 06:08:41 +0500

    Adding related objects with StackedInline.

--- inception/polls/admin.py
+++ inception/polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -7,6 +12,7 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
